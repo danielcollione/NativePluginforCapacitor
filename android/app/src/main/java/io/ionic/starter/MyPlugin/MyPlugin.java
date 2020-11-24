6a8b442803a757732f7ccddda81fe3c45302c918
danielcollione <daniel.collione22@gmail.com>
2020-11-24 18:48:17 -0300

    update
    declaração do plugin

---
+++ android/app/src/main/java/io/ionic/starter/MyPlugin/MyPlugin.java
@@ -0,0 +1,23 @@
+package io.ionic.starter.MyPlugin;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.NativePlugin;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+
+@NativePlugin
+public class MyPlugin extends Plugin{
+    public void load(){
+
+    }
+
+    @PluginMethod
+    public void echo(PluginCall call){
+        String value = call.getString("value");
+
+        JSObject ret = new JSObject();
+        ret.put("value", value);
+        call.success(ret);
+    }
+}
---
+++ src/app/plugin/MyPlugin.d.ts
@@ -0,0 +1,13 @@
+declare module "@capacitor/core" {
+    interface PluginRegistry {
+        MyPlugin: MyPlugin;
+    }
+}
+
+declare interface Echo{
+    value: string;
+}
+
+export interface MyPlugin{
+    echo(): Promise<Echo>
+}
